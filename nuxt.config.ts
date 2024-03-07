a6b1b1b74dfe4d1466d7e7be8d17359b44b00eee
Alex Quarrie <aquarrie@opentext.com>
2024-03-07 11:08:55 -0500

    commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  app : {
+    head : {
+      title : 'youronlinevariety',
+      meta : [
+        {name : 'description' , content :'Easy to use website to buy a random collection of items on the internet'}
+      ],
+      link: [
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+      ]
+    }
+  }
+})
