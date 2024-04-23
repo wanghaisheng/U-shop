c2a731350683b488338c1be773a27303af315c37
alexquar <alexanderquarrie@gmail.com>
2024-04-23 13:38:49 -0400

    about page done

--- pages/about.vue
+++ pages/about.vue
@@ -46,8 +46,8 @@
   <div class="bg-white">
     <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
       <div>
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Technical Specifications</h2>
-        <p class="mt-4 text-gray-500">The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated steel divider separates active cards from new ones, or can be used to archive important task lists.</p>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our Company</h2>
+        <p class="mt-4 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus cumque corporis, eos aperiam suscipit doloribus, qui pariatur distinctio perferendis aut exercitationem? Impedit repellat recusandae nostrum fugit voluptate deserunt quo, at reprehenderit sit assumenda quaerat natus reiciendis, corrupti veritatis minima et illum fugiat odio consectetur consequatur doloribus, soluta numquam ut? Illum!</p>
 
         <dl class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
           <div v-for="feature in features" :key="feature.name" class="border-t border-gray-200 pt-4">
@@ -58,9 +58,9 @@
       </div>
       <div class="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
         <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg" alt="Walnut card tray with white powder coated steel divider and 3 punchout holes." class="rounded-lg bg-gray-100" />
-        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg" alt="Top down view of walnut card tray with embedded magnets and card groove." class="rounded-lg bg-gray-100" />
-        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg" alt="Side of walnut card tray with card groove and recessed card area." class="rounded-lg bg-gray-100" />
-        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg" alt="Walnut card tray filled with cards and card angled in dedicated groove." class="rounded-lg bg-gray-100" />
+        <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg" alt="Top down view of walnut card tray with embedded magnets and card groove." class="rounded-lg bg-gray-100" />
+        <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Side of walnut card tray with card groove and recessed card area." class="rounded-lg bg-gray-100" />
+        <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Walnut card tray filled with cards and card angled in dedicated groove." class="rounded-lg bg-gray-100" />
       </div>
     </div>
   </div>
@@ -94,11 +94,11 @@
   { id: 3, name: 'Range of items', value: 'Widest' },
 ]
 const features = [
-  { name: 'Origin', description: 'Designed by Good Goods, Inc.' },
-  { name: 'Material', description: 'Solid walnut base with rare earth magnets and powder coated steel card cover' },
-  { name: 'Dimensions', description: '6.25" x 3.55" x 1.15"' },
-  { name: 'Finish', description: 'Hand sanded and finished with natural oil' },
-  { name: 'Includes', description: 'Wood card tray and 3 refill packs' },
-  { name: 'Considerations', description: 'Made from natural materials. Grain and color vary with each item.' },
+  { name: 'Produced', description: 'All products are created locally in Canada.' },
+  { name: 'Materials', description: 'We use only the best locally sourced materials.' },
+  { name: 'Sizes', description: 'We provide a wide range of size for applicable products.' },
+  { name: 'Shipping', description: 'We are proud to offer free shipping to our Canadian customers' },
+  { name: 'Warranty', description: 'All products have a lifetime warranty.' },
+  { name: 'Prices', description: 'We offer the most fair prices we can while paying providers and employees well.' },
 ]
   </script>
