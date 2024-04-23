7fceae71de4ac664337c9460fe9287d4afb6832d
alexquar <alexanderquarrie@gmail.com>
2024-04-23 13:29:38 -0400

    about us

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,72 @@
 <template>
-    <div>
-      <h2>About</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aperiam minima unde nemo harum quam maxime optio quos corrupti. Eum sapiente facere nemo, laborum ullam non cum fuga quas eveniet harum molestiae minus atque vel ratione illo quia, iure commodi dicta porro excepturi quam. Facere, commodi corrupti ipsam neque totam reprehenderit laboriosam laborum veniam est quo, repellat saepe impedit labore?</p>
-      <!-- <p>api response:</p>
-      <div>{{ data }}</div> -->
+  <div class="flex flex-col">
+    <h1 class="text-4xl md:text-7xl mx-auto my-10">About U-Shop</h1>
+  <section class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div class="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
+    <div class="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
+    <div class="mx-auto max-w-2xl lg:max-w-4xl">
+      <div class="mx-auto flex justify-center" >
+      <img class="h-12" src="@/public/favicon.ico" />
+      <h2>Honest Reviews</h2>
     </div>
-  </template>
+      <figure class="mt-10">
+        <blockquote class="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+          <p>“Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.”</p>
+        </blockquote>
+        <figcaption class="mt-10">
+          <div class="mt-4 flex items-center justify-center space-x-3 text-base">
+            <div class="font-semibold text-gray-900">John Doe</div>
+            <div class="text-gray-600">Valued Customer</div>
+          </div>
+        </figcaption>
+      </figure>
+      <figure class="mt-10">
+        <blockquote class="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+          <p>“Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.”</p>
+        </blockquote>
+        <figcaption class="mt-10">
+          <div class="mt-4 flex items-center justify-center space-x-3 text-base">
+            <div class="font-semibold text-gray-900">Jane Doe</div>
+            <div class="text-gray-600">Valued Customer</div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  </section>
+  <div class="bg-white py-24 my-5 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+        <div v-for="stat in stats" :key="stat.id" class="mx-auto flex max-w-xs flex-col gap-y-4">
+          <dt class="text-base leading-7 text-gray-600">{{ stat.name }}</dt>
+          <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">{{ stat.value }}</dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+  <div class="bg-white">
+    <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+      <div>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Technical Specifications</h2>
+        <p class="mt-4 text-gray-500">The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated steel divider separates active cards from new ones, or can be used to archive important task lists.</p>
+
+        <dl class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+          <div v-for="feature in features" :key="feature.name" class="border-t border-gray-200 pt-4">
+            <dt class="font-medium text-gray-900">{{ feature.name }}</dt>
+            <dd class="mt-2 text-sm text-gray-500">{{ feature.description }}</dd>
+          </div>
+        </dl>
+      </div>
+      <div class="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg" alt="Walnut card tray with white powder coated steel divider and 3 punchout holes." class="rounded-lg bg-gray-100" />
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg" alt="Top down view of walnut card tray with embedded magnets and card groove." class="rounded-lg bg-gray-100" />
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg" alt="Side of walnut card tray with card groove and recessed card area." class="rounded-lg bg-gray-100" />
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg" alt="Walnut card tray filled with cards and card angled in dedicated groove." class="rounded-lg bg-gray-100" />
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
   
   <style scoped>
     h2 {
@@ -27,4 +88,17 @@
     // const { data } = await useFetch('/api/ninja')
   
     // const { data } = await useFetch('/api/currency/GBP')
+    const stats = [
+  { id: 1, name: 'Deliveries in Canada', value: 'Free' },
+  { id: 2, name: 'Prices you can find', value: 'Cheepest' },
+  { id: 3, name: 'Range of items', value: 'Widest' },
+]
+const features = [
+  { name: 'Origin', description: 'Designed by Good Goods, Inc.' },
+  { name: 'Material', description: 'Solid walnut base with rare earth magnets and powder coated steel card cover' },
+  { name: 'Dimensions', description: '6.25" x 3.55" x 1.15"' },
+  { name: 'Finish', description: 'Hand sanded and finished with natural oil' },
+  { name: 'Includes', description: 'Wood card tray and 3 refill packs' },
+  { name: 'Considerations', description: 'Made from natural materials. Grain and color vary with each item.' },
+]
   </script>
