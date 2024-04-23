2b0d475b253b622925b5517f515cf556691972ac
alexquar <alexanderquarrie@gmail.com>
2024-04-23 09:20:53 -0400

    auth setup

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATzgcn6rs8y2BBziefD4mt9kkQqy-ZWJs",
+  authDomain: "u-shop-online.firebaseapp.com",
+  projectId: "u-shop-online",
+  storageBucket: "u-shop-online.appspot.com",
+  messagingSenderId: "510309422787",
+  appId: "1:510309422787:web:76625514fadfcad93c3476"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const projectAuth = firebase.auth()
+
+export { app, projectAuth }
