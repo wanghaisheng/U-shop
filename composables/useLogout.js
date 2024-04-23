667501695ddc4601cbabc48b349c63064c87265a
alexquar <alexanderquarrie@gmail.com>
2024-04-23 09:41:03 -0400

    useLogout

---
+++ composables/useLogout.js
@@ -0,0 +1,24 @@
+
+import { projectAuth } from '../firebase/config'
+
+const error = ref(null)
+const isPending = ref(null)
+const logout = async () => {
+  error.value = null
+    isPending.value = true
+  try {
+    await projectAuth.signOut()
+    isPending.value = false
+  }
+  catch(err) {
+    console.log(err.message)
+    error.value = err.message
+    isPending.value = false
+  }
+}
+
+const useLogout = () => {
+  return { error, logout, isPending}
+}
+
+export default useLogout
