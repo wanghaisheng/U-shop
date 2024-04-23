da87774f7fabb31bfef2013858e48c393ebe353e
alexquar <alexanderquarrie@gmail.com>
2024-04-23 13:55:16 -0400

    404

--- error.vue
+++ error.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="mt-7 max-w-sm mx-auto text-center card">
+  <div class="flex justify-center align-center">
+  <div class="mt-7 p-10 max-w-md lg:max-w-xl text-center card">
     <p class="mt-7 text-7xl font-bold">{{error.statusCode}}</p>
-    <p class="mt-7 text-6xl">Ooops.</p>
+    <p class="mt-7 text-6xl">Ooops...</p>
+    <p class="mt-7 text-3xl">Something went wrong :(</p>
     <p class="mt-7">{{error.message}}</p>
     <button class="btn my-7" @click="handleError">Go Home...</button>
   </div>
+</div>
 </template>
 
 <script setup>
