c87d5210e4bd7710acfdeca57476d9e91bbbe813
alexquar <alexanderquarrie@gmail.com>
2024-04-23 08:51:32 -0400

    signup page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -25,13 +25,13 @@
         </div>
 
         <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+          <button type="submit" class="flex w-full justify-center rounded-md btn px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
         </div>
       </form>
 
       <p class="mt-10 text-center text-sm text-gray-500">
         Don't have an account?
-        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign in</a>
+        <Nuxt-Link to="/auth/signup" class="font-semibold leading-6 btn">Sign up</Nuxt-Link>
       </p>
     </div>
   </div>
